86fb0f63a2db8507bce73eb3caf402ded2a45c79
anonim <djumppp@mail.ru>
2023-06-14 15:49:19 +0300

    ex5

---
+++ Example/ex5/Program.cs
@@ -0,0 +1,7 @@
+﻿ // случайное число 1 2 3 4 ..... 99
+ double numberA = new Random().Next(1, 100);
+ Console.WriteLine(numberA);
+ double numberB = new Random().Next(1, 100);
+ Console.WriteLine(numberB);
+ double result = numberA + numberB;
+ Console.WriteLine(result);
