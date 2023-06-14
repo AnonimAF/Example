60d70b8c34432474efd0acf8121ee87e1d34c32e
anonim <djumppp@mail.ru>
2023-06-14 15:19:56 +0300

    ex3

--- Example/Ex3/Program.cs
+++ Example/Ex3/Program.cs
@@ -1,4 +1,10 @@
-﻿int numberA = 3;
+﻿Console.WriteLine("Enter your name: ");
+string username = Console.ReadLine();
+Console.Write(username);
+Console.WriteLine(" Введите ваши цифры");
+int numberA = 3;
 int numberB = 5;
-int result = numberA + numberB;
-Console.WriteLine(result);
+int result = numberA * numberB;
+Console.Write("Ваш ответ равен: ");
+Console.Write(result);
+
